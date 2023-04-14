a9e7ace45ecf7b7f423b7dff88a1f2c3ead8efab
svitlanakokorska <s.kokorskaya@gmail.com>
2023-04-14 13:37:33 +0300

    стилі

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,8 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        flexDirection: 'column'
       }}
     >
   
